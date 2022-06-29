a323f30a3660415cf3a45e96baa97a19f16aad86
vladpustovit <pustovitvladyslav@gmail.com>
2022-06-29 23:06:46 +0200

    1

---
+++ Practice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        public static void Main()
+        {
+            double pi = Math.PI;
+            int tenThousand = 10000;
+            float tenThousandPi = (float)pi * tenThousand;
+            int roundedTenThousandPi = (int)Math.Round(tenThousandPi);
+            int integerPartOfTenThousandPi = (int)tenThousandPi;
+            Console.WriteLine(roundedTenThousandPi);
+            Console.WriteLine(integerPartOfTenThousandPi);
+        }
+    }
+}
